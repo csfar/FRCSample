8c844b7e72971d66d5b5c347ef0dbf6d3ea332d3
csfar <csf.artur@gmail.com>
2021-07-21 17:08:22 -0300

    Initial Commit

---
+++ SampleSxasdas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSxasdas
+//
+//  Created by Artur Carneiro on 21/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
